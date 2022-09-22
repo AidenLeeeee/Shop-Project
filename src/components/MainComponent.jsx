4b26f8c43bbcb355bcc9d7ed273aa26bdf038dc4
AidenLeeeee <hoo8799@naver.com>
2022-09-23 03:17:02 +0900

    Bug fixed

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,9 +1,30 @@
 import CardComponent from "./CardComponent";
 import axios from "axios";
+import styled from "styled-components";
+import { useEffect } from "react";
+
+const StyledButton = styled.button`
+  background-color: #fff;
+  color: grey;
+  padding: 10px;
+  margin: 20px 0px;
+  border: 1px solid #c1c1c1;
+  border-radius: 10px;
+  transition: 0.2s ease-in-out;
+  &:hover {
+    border-color: #000;
+    color: #000;
+  }
+`;
+
+let moreBtnCount = 0;
+let showBtn = true;
+let loading = false;
 
 export default function MainComponent(props) {
   const products = props.products;
   const setProducts = props.setProducts;
+
   return (
     <>
       <div className="main-bg"></div>
@@ -15,21 +36,37 @@ export default function MainComponent(props) {
           })}
         </div>
       </div>
-      <button
-        onClick={() => {
-          axios
-            .get("https://codingapple1.github.io/shop/data2.json")
-            .then((res) => {
-              const copyProducts = [...products, ...res.data];
-              setProducts(copyProducts);
-            })
-            .catch(() => {
-              console.log("Failed");
-            });
-        }}
-      >
-        More
-      </button>
+      {showBtn ? (
+        <StyledButton onClick={loadMoreProducts}>Show more</StyledButton>
+      ) : null}
+      {loading ? <StyledButton>loading...</StyledButton> : null}
     </>
   );
+
+  function loadMoreProducts() {
+    if (moreBtnCount === 0) {
+      axiosRequest(moreBtnCount + 2);
+    } else if (moreBtnCount === 1) {
+      showBtn = false;
+      axiosRequest(moreBtnCount + 2);
+    } else {
+      return null;
+    }
+  }
+
+  function axiosRequest(fileNum) {
+    axios
+      .get("https://codingapple1.github.io/shop/data" + fileNum + ".json")
+      .then((res) => {
+        loading = true;
+        const copyProducts = [...products, ...res.data];
+        setProducts(copyProducts);
+        moreBtnCount += 1;
+        loading = false;
+      })
+      .catch(() => {
+        loading = false;
+        console.log("Connection Failed.");
+      });
+  }
 }
